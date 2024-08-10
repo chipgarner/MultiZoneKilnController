a84feb1fa7e737fd5bb730ba14e22c2d1c72761e
Chip Garner <chip@garnertotalenergy.com>
2024-08-10 14:05:01 -0700

    Two zones, fast bisque uses 90C

--- config.py
+++ config.py
@@ -9,20 +9,19 @@ log_format = '%(asctime)s %(levelname)s %(name)s: %(message)s'
 logging.basicConfig(level=log_level, format=log_format)
 log = logging.getLogger(__name__)
 
-simulating = True
+simulating = False
 
 if simulating:
     zone_temps = ZoneTemps()
     sim_speed_up_factor = 1
 else:
     try:
-        # zone1 = {'name': 'Top',
-        #          'temperature_sensor': Max31855(board.D5),
-        #          'power_controller': SSR(board.D17)}
-        zone1 = {'name': 'Bottom',
+        zone1 = {'name': 'Top',
+                 'temperature_sensor': Max31855(board.D5),
+                 'power_controller': SSR(board.D17)}
+        zone2 = {'name': 'Bottom',
                  'temperature_sensor': Max31856(board.D6),
                  'power_controller': SSR(board.D27)}
-        zone2 = None
         zone3 = None
         zone4 = None
     except AttributeError as err:
